ca04b978993e26c2f82b10808e499af906ba3d2a
davidmarkal <144433578+davidmarkal@users.noreply.github.com>
2025-04-24 18:00:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you hahahaha" }');
+    res.send('{ "response": "Hello From Thetips4you k8s" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World k8s" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works k8s!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
